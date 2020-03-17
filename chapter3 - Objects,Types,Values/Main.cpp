a5623dbd507f8de657187d8d389ee839858cdb9f
faz <45979860+FinFarrell@users.noreply.github.com>
2020-03-17 21:09:56 +0000

    step 1 - getting the recipitent of the letter from the user

--- chapter3 - Objects,Types,Values/Main.cpp
+++ chapter3 - Objects,Types,Values/Main.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
-	cout << "hello";
+	// the drill is to create a letter based on user input
+
+	// step 1 - getting the recipitent of the letter from the user
+	string first_name;
+	cout << "Who are you writing too?";
+	cin >> first_name;
+	cout << "Dear " << first_name << ", " << "\n ";
+
 	return 0;
 }
 
