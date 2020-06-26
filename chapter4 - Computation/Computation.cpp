cd4da737748819630fac5ba395a614ba01041344
faz <45979860+FinFarrell@users.noreply.github.com>
2020-06-26 12:38:45 +0100

    add everything then basic i/o loop

---
+++ chapter4 - Computation/Computation.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h";
+
+int main()
+{
+	// dril time, learnt about basic statements which is pretty simple and about vectors, essentially the same as arrays from python
+
+	// Step 2 - while loop reading 2 ints and writing them
+	int a;
+	int b;
+	cout << "enter 2 integers, then they will be printed, when you wish to exit, enter |" << "\n";
+	while (cin >> a, cin>>b)
+	{
+		cout << a << "\n" << b << "\n";
+	}
+
+
+}
