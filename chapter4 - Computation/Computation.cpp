243aaf9105c5ae8fa77558c289023031bcfccb41
faz <45979860+FinFarrell@users.noreply.github.com>
2020-06-26 12:59:21 +0100

    step 6 - overhall to create a check for smallest and largest

--- chapter4 - Computation/Computation.cpp
+++ chapter4 - Computation/Computation.cpp
@@ -8,33 +8,31 @@ int main()
 	// Step 2 and 3 - writing out the smaller value then the larger one, and checking if they are equal
 	// Steo 4 - changing all to doubles
 	// Step 5 - changed checks for similar nubmers and equal numbers
+	// Step 6 - overhaul to make one double be added and that being checked for smallest and largest
 	double a;
-	double b;
+	double smallest = 10000000;
+	double largest = 0;
 	bool a_larger;
 	cout << "enter 2 integers, then they will be printed, when you wish to exit, enter |" << "\n";
-	while (cin >> a, cin>>b)
+	while (cin >> a)
 	{
-		// basic logic to print them out in order of size
-		if (a < b) 
+		// basic logic see if the number is bigger or smaller
+		if (a > largest)
 		{
-			cout << a << "\n" << b << "\n";
-			a_larger = false;
+			cout << "largest so far, bigger than " << largest << "\n";
+			largest = a;
 		}
-		else if (b < a)
+		else if (a < smallest)
 		{
-			cout << b << "\n" << a << "\n";
-			a_larger = true;
+			cout << "smallest so far, smaller than " << smallest << "\n";
+			smallest = a;
 		}
+		else
+		{
+			cout << "nothing special about this number";
+		}
+		
 
-		// checking if they are similar
-		if (a_larger && a - b <= 1.0 && a-b != 0 || !a_larger && b - a <= 1.0 && b-a != 0)
-		{
-			cout << "SIMILAR NUMBERS" << "\n";
-		}
-		else if (a - b == 0)
-		{
-			cout << "EQUAL" << "\n";
-		}
 
 	}
 
