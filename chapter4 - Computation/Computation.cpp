0db6cbfa1605f94f0fd3f23153adecfbff0094fd
faz <45979860+FinFarrell@users.noreply.github.com>
2020-06-26 12:42:07 +0100

    step 2 and 3 - basic logic with if statements

--- chapter4 - Computation/Computation.cpp
+++ chapter4 - Computation/Computation.cpp
@@ -4,14 +4,29 @@ int main()
 {
 	// dril time, learnt about basic statements which is pretty simple and about vectors, essentially the same as arrays from python
 
-	// Step 2 - while loop reading 2 ints and writing them
+	// Step 1 - while loop reading 2 ints and writing them
+	// Step 2 and 3 - writing out the smaller value then the larger one, and checking if they are equal
 	int a;
 	int b;
 	cout << "enter 2 integers, then they will be printed, when you wish to exit, enter |" << "\n";
 	while (cin >> a, cin>>b)
 	{
-		cout << a << "\n" << b << "\n";
+		if (a < b) 
+		{
+			cout << a << "\n" << b << "\n";
+		}
+		else if (b > a)
+		{
+			cout << b << "\n" << a << "\n";
+		}
+		else
+		{
+			cout << "Numbers are equal" << "\n";
+		}
+		
 	}
+
+
 
 
 }
